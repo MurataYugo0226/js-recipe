ace9ffa2554a2fab5cec3eee45d76bf7619d8886
Murata Yugo <m.yugo022605@gmail.com>
2025-10-09 09:45:19 +0900

    [３の倍数]を学習しました

--- 01_テキスト/00_JavaScriptの式と演算子/main.js
+++ 01_テキスト/00_JavaScriptの式と演算子/main.js
@@ -1,7 +1,16 @@
 // TRY: 練習問題１
+let x = 57052 + 57054
+let y = "114" + "106"
 
+console.log(x)
+console.log(y)
 // TRY: 練習問題２
-
+let z = 5
+z += 2
+z *= 5
+z = 100 % z
+z /= 6
+console.log(z)
 // TRY: 練習問題３
 let happiness = 0 // 0 から 100 までの数値を代入してください
 let sleepiness = 0 // 0 から 100 までの数値を代入してください
@@ -12,6 +21,9 @@ const isSleepy = sleepiness > 90
 const isNotHappyAndSleepy = !isHappy && isSleepy
 
 // 以下のコメントアウトを外して確認してみましょう
-// console.log("isNotHappyAndSleepy" + "は" + true + "ですか？", isNotHappyAndSleepy)
-// console.log("isHappy", isHappy)
-// console.log("isSleepy", isSleepy)
+console.log(
+  "isNotHappyAndSleepy" + "は" + true + "ですか？",
+  isNotHappyAndSleepy,
+)
+console.log("isHappy", isHappy)
+console.log("isSleepy", isSleepy)
--- 01_テキスト/01_JavaScriptのオブジェクト/main.js
+++ 01_テキスト/01_JavaScriptのオブジェクト/main.js
@@ -1,21 +1,13 @@
-const hikakin = {
-  name: "ヒカキン",
-  age: 24,
-  hobbies: ["ゲーム", "動画編集", "猫と遊ぶこと"],
-  isHappy: true,
-  address: {
-    country: "日本",
-    city: "東京",
-  },
-  sayHello: function () {
-    console.log("ブンブンハロー YouTube")
-  },
-}
-
 // TRY: 練習問題１
 // hikakin オブジェクト を真似して自分のプロフィールをオブジェクトで作ってください。
 // オブジェクトにはたくさんのプロパティを追加してください。
+const me = {
+  name: "yugo",
+  age: 20,
+  hobbies: ["筋トレ", "スポーツ観戦"],
+}
 
+console.log(me)
 // const me = { name: 'あなたのなまえ' }
 
 // ブラウザのコンソールを開いてオブジェクトが正しく作れているか確認してみましょう👀
@@ -24,3 +16,11 @@ const hikakin = {
 
 // TRY: 練習問題２
 // hikakin オブジェクトに入っている sayHello メソッドを実行してください。
+const hikakin = {
+  name: "ヒカキン",
+  age: 24,
+  sayHello: function () {
+    console.log("ブンブンハロー Youtube")
+  },
+}
+console.log(hikakin.sayHello)
